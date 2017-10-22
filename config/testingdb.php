9764263d896c95d714069e16602e2fbdd62bfb69
Jordan <jordanmay10@hotmail.com>
2017-10-22 22:30:24 +0000

    Doctor Dashboard - Not completed yet

--- config/testingdb.php
+++ config/testingdb.php
@@ -1,17 +1,5 @@
 <?php
-    $con = mysqli_connect("127.0.0.1", "gordons", "", "c9");
-    
-    $name = $_POST["name"];
-    $age = $_POST["age"];
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    $statement = mysqli_prepare($con, "INSERT INTO appUsers  (name, username, age, password) VALUES (?, ?, ?, ?)");
-    mysqli_stmt_bind_param($statement, "ssis", $name, $username, $age, $password);
-    mysqli_stmt_execute($statement);
-    
-    $response = array();
-    $response["success"] = true;  
-    
-    echo json_encode($response);
+    $con = mysqli_connect();
+// dont mind this file
 
 ?>
