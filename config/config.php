0229e784c5202c013bb9f47007a568626e9ee501
Jordan <jordanmay10@hotmail.com>
2017-10-22 22:28:47 +0000

    Doctor Dashboard - Not completed yet

--- config/config.php
+++ config/config.php
@@ -2,12 +2,14 @@
 
 // Database Settings
 
-return array(
-        'user' => 'gordons',
-        'pass' => '',
-        'host' => '127.0.0.1',
-        'db' => 'c9'  
-)
+$Host = "127.0.0.1";
+$User = "gordons";
+$Pass = "";
+$Name = "c9";
+
+//connects to our database
+$DBcon = new MySQLi($Host,$User,$Pass,$Name);
+
 ?>
 
 
