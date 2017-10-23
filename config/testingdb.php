8c3d0d82b752ed0baadb1c382b93d855e5910368
Jordan <jordanmay10@hotmail.com>
2017-10-23 10:01:39 +0000

    Styling fix + code for root privilege

--- config/testingdb.php
+++ config/testingdb.php
@@ -1,5 +1,20 @@
 <?php
-    $con = mysqli_connect();
-// dont mind this file
+    // A simple PHP script demonstrating how to connect to MySQL.
+    // Press the 'Run' button on the top to start the web server,
+    // then click the URL that is emitted to the Output tab of the console.
 
-?>
+    $servername = '127.0.0.1';
+    $username = 'gordons';
+    $password = "";
+    $database = "c9";
+    $dbport = 3306;
+
+    // Create connection
+    $db = new mysqli($servername, $username, $password, $database, $dbport);
+
+    // Check connection
+    if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
+    } 
+    echo "Connected successfully (".$db->host_info.")";
+// mysql -u root -p run this 
