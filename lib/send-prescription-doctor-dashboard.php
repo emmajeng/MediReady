344b9a722fe041d984bc7aa00963f834d00d5316
Jordan <jordanmay10@hotmail.com>
2017-11-20 13:02:49 +0000

    Style changes

--- lib/send-prescription-doctor-dashboard.php
+++ lib/send-prescription-doctor-dashboard.php
@@ -1,47 +1,10 @@
 <?php
 require_once 'config.php';
-/*
-include 'show-patient-modal.php';
-
-echo "Welcome ", $gPatientID, "!";
-*/
-
-/* get the patient id */
-
-/*
-if (isset($_POST['btn-sendMed']))
-{
-    
-$med = array(); 
-$amt = array();
-
-$id = $_POST['patient_id'];
-echo $id;
-
-$med = $_POST['medication'];
-$amt = $_POST['amount'];
-
-
-
-foreach ($med as $val_med) {
-    echo "$val_med <br>";
-}
-
-foreach ($amt as $val_amt) {
-    echo "$val_amt <br>";
-}
-
-
-}
-*/
-
-/* TESTING QUERY */
-
 
 if (isset($_POST['btn-sendMed']))
 {
-    
-$med = array(); 
+
+$med = array();
 $amt = array();
 
 $id = $_POST['patient_id'];
@@ -61,16 +24,22 @@ $DBcon->query($presciption);
 
 $orderID = $DBcon->insert_id;
 
-echo $orderID;
+//echo $orderID;
 
 foreach(array_combine($med, $amt) as $val_med => $val_amt) {
         $sql = "INSERT INTO `prescription_details_table`(`prescription_id`, `med_name`, `med_amount`) VALUES ('$orderID','$val_med','$val_amt')";
         $DBcon->query($sql);
 }
-
-$orderID2 = $DBcon->insert_id;
 /* check of med name is empty and amount from previous record if so delete */
+/* Fixes the issue when php inserts into database twice */
+$orderID2 = $DBcon->insert_id;
+//echo $orderID2;
+$deleteQuery = "delete from prescription_details_table where prescdetails_id = '". $orderID2 ."'";
+$DBcon->query($deleteQuery);
 
 }
+/* for now, plan to implement ajax and pop up a success message */
+header("Location: ../doctor_dashboard.php");
+
 
 ?>
