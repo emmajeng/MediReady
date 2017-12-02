46ab96c5b98eaf839ffdcb2efd804772bf3d22c1
emmajeng <x15575767@gmail.com>
2017-12-02 16:22:46 +0000

    Mapbox JS

--- js/mapbox.js
+++ js/mapbox.js
@@ -1,3 +1,3 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiZW1tYWplbmciLCJhIjoiY2phbzNkdnRqM2kzejMzcGx6aXRnNGEzZCJ9.B2v7VV1Go9UVRv0m-YEDOw';
 
-var map = L.mapbox.map('map', 'mapbox.streets-v9');
+var map = L.mapbox.map('map', 'mapbox.streets');
