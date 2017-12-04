1514f9863d1e37078f880e73a35d0de186cb24d1
NCIR-CAMPUS\x15575767 <x15575767@student.ncirl.ie>
2017-12-04 14:32:39 +0000

    Added most up-to-date Mapbox Version

--- deliveryman_dashboard.php
+++ deliveryman_dashboard.php
@@ -54,6 +54,9 @@ else {
     <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+    <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.42.2/mapbox-gl.js'></script>
+    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.42.2/mapbox-gl.css' rel='stylesheet' />
+
 
     <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet" />
@@ -87,53 +90,99 @@ else {
   <!--Tabs for Patient Dashboard-->
     <div class="dashboard-container">
       <div id="tab-buttons">
-        <button onclick="setTabContents('tab-1')" id="map" class="tab-1 active" >Map</button>
+        <button onclick="setTabContents('tab-1')" id="mapDisplay" class="tab-1 active" >Map</button>
         <button onclick="setTabContents('tab-2')" id="directions" class="tab-2" >Directions</button>
       </div>
       <div id="tab-contents">
         <div class="tab-1 active">
-          <!-- Google Map insert -->
-          <div id="googleMap"></div>
+          <div id="mapContainer">
+            <div id="map"></div>
+            <script>
+            mapboxgl.accessToken = 'pk.eyJ1IjoiZW1tYWplbmciLCJhIjoiY2phbzNkdnRqM2kzejMzcGx6aXRnNGEzZCJ9.B2v7VV1Go9UVRv0m-YEDOw';
+            var map = new mapboxgl.Map({
+                container: 'map', // container id
+                style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+                center: [-74.50, 40], // starting position [lng, lat]
+                zoom: 9 // starting zoom
+            });
+            </script>
+          </div>
+           
+            
         </div>
+        
         <div class="tab-2">
           <h4>
-            Directions will be shown for the driver here
+            <ul>
+            <li>1. Head north on Jervis St toward Mary St</li>
+            <li>2. Continue on North Rd to your destination</li>
+            <li>3. Take R804, R805, Aughrim St/R806 and N Circular Rd/R101 to North Rd</li>
+            </ul>
           </h4>
         </div>
       </div>
     </div>
-
-
+      
+    <!-- Update route button -->
+    <button name="calc_route" class="btn btn-xl btn-mrg" id="calc_route">Update Route</button>
+     
+    
     <!-- Orders Table -->
     <div class="dashboard-container bottom-container">
       <table class="table-fill">
         <thead>
         <tr>
-        <th class="text-left">Orders</th>
+        <th class="text-left">Name</th>
         <th class="text-left">Address</th>
+        <th class="text-left">Price</th>
+        <th class="text-left">Status</th>
         </tr>
         </thead>
         <tbody class="table-hover">
-          <tr>
-          <td class="text-left">January</td>
-          <td class="text-left">$ 50,000.00</td>
-          </tr>
-          <tr>
-          <td class="text-left">February</td>
-          <td class="text-left">$ 10,000.00</td>
-          </tr>
-          <tr>
-          <td class="text-left">March</td>
-          <td class="text-left">$ 85,000.00</td>
-          </tr>
-          <tr>
-          <td class="text-left">April</td>
-          <td class="text-left">$ 56,000.00</td>
-          </tr>
-          <tr>
-          <td class="text-left">May</td>
-          <td class="text-left">$ 98,000.00</td>
-          </tr>
+          <?php
+                  require_once 'lib/config.php';
+
+                  // Create connection
+                  // Check connection
+                  if ($DBcon->connect_error)
+                  {
+                    die("Connection failed: " . $DBcon->connect_error);
+                  }
+
+                  $sql = "SELECT * FROM orders_table WHERE status = 'Delivery'";
+                  $result = $DBcon->query($sql);
+
+                  if ($result->num_rows > 0)
+                  {
+                    // output data of each row
+                    while($row = $result->fetch_assoc())
+                    {
+                      $patient = "SELECT * FROM patient_table WHERE patient_id =" . $row['patient_id'].";";
+                      $details = $DBcon->query($patient);
+                      
+                      while($row1 = $details->fetch_assoc())
+                    {
+                      
+                      echo "<tr>";
+                        echo "<td>" . $row1['patient_fname']. "</td>";
+                        echo "<td>" . $row1['patient_address']. "</td>";
+                        echo "<td>" . $row['price']. "</td>";
+                        echo "<td><button class='btn btn-success btn-block'>" . $row['status']. "</button></td>";
+                      echo "</tr>";
+                    }
+                    }
+                  }
+                  else
+                  {
+                    echo "<tr>";
+                        echo "<td>N</td>";
+                        echo "<td>A</td>";
+                        echo "<td>D</td>";
+                        echo "<td><button class='btn btn-success btn-block'>Status</button></td>";
+                      echo "</tr>";
+                  }
+                  
+                ?>
         </tbody>
       </table>
     </div>
@@ -206,8 +255,6 @@ else {
     <!-- Google Map function and get location -->
     <script language="javascript" type="text/javascript" src="js/map.js"></script>
 
-    <!-- API Key for Map -->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3u7zBjNuRP74J7XOKA0nGAQyNeAY2_vc&callback=myMap"></script>
   </body>
 
 </html>
