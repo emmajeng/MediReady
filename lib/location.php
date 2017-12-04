4f88ef4fe11b062b081a886b3183b581eeb20757
NCIR-CAMPUS\x15575767 <x15575767@student.ncirl.ie>
2017-12-04 14:33:47 +0000

    Pulling the lat and long from the users address - Chris's Commit

---
+++ lib/location.php
@@ -0,0 +1,38 @@
+<!-- PHP display info on modal in doctor dashboard -->
+
+<?php
+/*
+include('send-prescription-doctor-dashboard.php');
+ */
+require_once 'config.php';
+
+
+    $query = "SELECT patient_address
+                FROM patient_table
+                INNER JOIN orders_table ON patient_table.patient_id = orders_table.patient_id";
+    
+    $result = mysqli_query($DBcon, $query);
+    
+    $locations = array(array());
+      
+    if($result->num_rows > 0)
+    {
+        while($row = mysqli_fetch_array($result))
+        {
+            $address = $row["patient_address"];
+            $address = str_replace(" ", "+", $address);
+            $region = "Ireland";
+            
+            $json = file_get_contents("http://maps.google.com/maps/api/geocode/json?address=$address&sensor=false&region=$region");
+            $json = json_decode($json);
+            
+            $lat = $json->{'results'}[0]->{'geometry'}->{'location'}->{'lat'};
+            $long = $json->{'results'}[0]->{'geometry'}->{'location'}->{'lng'};
+ 
+            $latlng = array($lat, $long);
+            
+            array_push($locations, $latlng);
+        }
+        print_r($locations);
+    }
+?>
